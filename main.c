05140370f2555103a689b9c7ad94f4f3ef39889a
Ricardo Dinis <basalto@gmail.com>
2023-12-16 17:04:10 +0000

    Refactoring

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
-const char *filepath = "A20231207.1715+0000-1730+0000_SubNetwork=ONRM_ROOT_MO,SubNetwork=RAN,SubNetwork=NODE,SubNetwork=AV_AVEIRO,MeContext=NAV002B2,ManagedElement=NAV002B2_celltracefile_DUL1_1.bin";
+const char *filepath = "sample_files/A20231207.1715+0000-1730+0000_SubNetwork=ONRM_ROOT_MO,SubNetwork=RAN,SubNetwork=NODE,SubNetwork=AV_AVEIRO,MeContext=NAV002B2,ManagedElement=NAV002B2_celltracefile_DUL1_1.bin";
 
 enum RecordType
 {
-    HEADER,
-    SCANNER,
-    EVENT,
-    FOOTER
+    HEADER = 0,
+    SCANNER = 3,
+    EVENT = 4,
+    FOOTER = 5
 };
 
 typedef struct RecordLenType
@@ -21,29 +22,19 @@ typedef struct RecordLenType
 typedef struct CTRHeader
 {
     uint16_t length;
-    uint16_t type;
-    uint8_t version[5];
-    uint8_t pm_version[13];
-    uint8_t pm_revision[5];
-    uint16_t year;
-    uint8_t month;
-    uint8_t day;
-    uint8_t hour;
-    uint8_t minute;
-    uint8_t second;
-    uint8_t ne_user_label[128];
-    uint8_t ne_logical_label[255];
+    uint8_t file_version[6];       //  5 bytes + termination char
+    uint8_t pm_version[14];        // 13 bytes + termination char
+    uint8_t pm_revision[6];        //  5 bytes + termination char
+    uint8_t date_time[20];         //  7 bytes + termination char (yyyy-mm-dd hh:mm:ss)
+    uint8_t ne_user_label[129];    // 128 bytes + termination char
+    uint8_t ne_logical_label[256]; // 255 bytes + termination char
     void *next;
 } CTRHeader;
 
 typedef struct CTRScanner
 {
     uint16_t length;
-    uint16_t type;
-    uint8_t hour;
-    uint8_t minute;
-    uint8_t second;
-    uint16_t milisecond;
+    uint8_t timestamp[13];
     uint8_t scannerid[3];
     uint8_t status[1];
     uint8_t padding[3];
@@ -53,7 +44,6 @@ typedef struct CTRScanner
 typedef struct CTREvent
 {
     uint16_t length;
-    uint16_t type;
     int id;
     uint8_t parameters[255];
     void *next;
@@ -62,7 +52,6 @@ typedef struct CTREvent
 typedef struct CTRFooter
 {
     uint16_t length;
-    uint16_t type;
     uint16_t year;
     uint8_t month;
     uint8_t day;
@@ -73,14 +62,49 @@ typedef struct CTRFooter
     void *next;
 } CTRFooter;
 
+typedef struct CTRStruct
+{
+    struct CTRStruct *next; // Next structure in the linked list
+    enum RecordType type;   // Indicates which of the union fields is valid
+    union
+    {
+        struct CTRHeader header;
+        struct CTRScanner scanner;
+        struct CTREvent event;
+        struct CTRFooter footer;
+    };
+} CTRStruct;
+
+int RecordTypeValid(int type)
+{
+    int valid = 0;
+
+    switch (type)
+    {
+    case HEADER:
+    case SCANNER:
+    case EVENT:
+    case FOOTER:
+        valid = 1;
+    };
+
+    return valid;
+}
+
 /* CHAR_BIT == 8 assumed */
 uint16_t le16_to_cpu(const uint8_t *buf)
 {
     return ((uint16_t)buf[0]) | (((uint16_t)buf[1]) << 8);
 }
+
 uint16_t be16_to_cpu(const uint8_t *buf)
 {
     return ((uint16_t)buf[1]) | (((uint16_t)buf[0]) << 8);
+}
+
+uint32_t be32_to_cpu(const uint8_t *buf)
+{
+    return ((uint32_t)buf[2] | (uint32_t)buf[1] << 8 | (uint32_t)buf[0] << 16);
 }
 
 void cpu_to_le16(uint8_t *buf, uint16_t val)
@@ -88,103 +112,126 @@ void cpu_to_le16(uint8_t *buf, uint16_t val)
     buf[0] = (val & 0x00FF);
     buf[1] = (val & 0xFF00) >> 8;
 }
+
 void cpu_to_be16(uint8_t *buf, uint16_t val)
 {
     buf[0] = (val & 0xFF00) >> 8;
     buf[1] = (val & 0x00FF);
 }
 
-int read_header(CTRHeader *hdr, uint16_t len, FILE *fp)
+void scan_string_from_buf(uint8_t *target_var, uint8_t *buf, uint16_t *buf_pos, uint16_t size)
 {
-    hdr->length = len;
-    hdr->type = 0;
+    for (int i = 0; i < size; i++)
+    {
+        target_var[i] = buf[*buf_pos + i];
+    }
+    target_var[size + 1] = '\0';
+    *buf_pos = *buf_pos + size;
+}
 
-    uint8_t buf[len - 4];
+void scan_bytes_from_buf(uint8_t *target_var, uint8_t *buf, uint16_t *buf_pos, uint16_t size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        target_var[i] = buf[*buf_pos + i];
+    }
+    *buf_pos = *buf_pos + size;
+}
+
+void scan_date_time(uint8_t *target_var, uint8_t *buf, uint16_t *buf_pos)
+{
+    snprintf(target_var, 20,
+             "%04d-%02d-%02d %02d:%02d:%02d",
+             be16_to_cpu(buf + *buf_pos),
+             (uint8_t)buf[*buf_pos + 2],
+             (uint8_t)buf[*buf_pos + 3],
+             (uint8_t)buf[*buf_pos + 4],
+             (uint8_t)buf[*buf_pos + 5],
+             (uint8_t)buf[*buf_pos + 6]);
+    *buf_pos = *buf_pos + 7;
+}
+
+void scan_timestamp(uint8_t *target_var, uint8_t *buf, uint16_t *buf_pos)
+{
+    snprintf(target_var, 13,
+             "%02d:%02d:%02d:%03d",
+             (uint8_t)buf[*buf_pos],
+             (uint8_t)buf[*buf_pos + 1],
+             (uint8_t)buf[*buf_pos + 2],
+             be16_to_cpu(buf + *buf_pos + 3));
+    *buf_pos = *buf_pos + 5;
+}
+
+int get_file_lenght(FILE *fp)
+{
+    int lenght = 0;
+    fseek(fp, 0L, SEEK_END);
+    lenght = ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
+    return lenght;
+}
+
+int get_file_pos(FILE *fp)
+{
+    return ftell(fp);
+}
+
+int read_header(CTRStruct *ptr, uint16_t len, FILE *fp)
+{
+    ptr->type = HEADER;
+    ptr->header.length = len;
+
+    uint16_t buf_pos = 0;
+    uint8_t buf[len - 4]; // first 4 bytes of buf (type+length) already read from file
 
     if (fread(buf, 1, sizeof buf, fp) != sizeof buf)
         return -1;
 
-    for (int i = 0; i <= 4; i++)
-    {
-        hdr->version[0 + i] = buf[i];
-    }
-
-    for (int i = 0; i <= 12; i++)
-    {
-        hdr->pm_version[0 + i] = buf[5 + i];
-    }
-
-    for (int i = 0; i <= 4; i++)
-    {
-        hdr->pm_revision[0 + i] = buf[18 + i];
-    }
-
-    hdr->year = be16_to_cpu(buf + 23);
-    hdr->month = (uint8_t)buf[25];
-    hdr->day = (uint8_t)buf[26];
-    hdr->hour = (uint8_t)buf[27];
-    hdr->minute = (uint8_t)buf[28];
-    hdr->second = (uint8_t)buf[29];
-
-    for (int i = 0; i <= 128; i++)
-    {
-        hdr->ne_user_label[0 + i] = buf[30 + i];
-    }
-
-    for (int i = 0; i <= 255; i++)
-    {
-        hdr->ne_logical_label[0 + i] = buf[158 + i];
-    }
+    scan_string_from_buf(ptr->header.file_version, buf, &buf_pos, 5);
+    scan_string_from_buf(ptr->header.pm_version, buf, &buf_pos, 13);
+    scan_string_from_buf(ptr->header.pm_revision, buf, &buf_pos, 5);
+    scan_date_time(ptr->header.date_time, buf, &buf_pos);
+    scan_string_from_buf(ptr->header.ne_user_label, buf, &buf_pos, 128);
+    scan_string_from_buf(ptr->header.ne_logical_label, buf, &buf_pos, 255);
 
     return 0;
 }
 
-int read_scanner(CTRScanner *scan, uint16_t len, FILE *fp)
+int read_scanner(CTRStruct *ptr, uint16_t len, FILE *fp)
 {
-    scan->length = len;
-    scan->type = 3;
+    ptr->type = SCANNER;
+    ptr->scanner.length = len;
 
-    uint8_t buf[len - 4];
+    uint16_t buf_pos = 0;
+    uint8_t buf[len - 4]; // first 4 bytes of buf (type+length) already read from file
 
     if (fread(buf, 1, sizeof buf, fp) != sizeof buf)
         return -1;
 
-    scan->hour = (uint8_t)buf[0];
-    scan->minute = (uint8_t)buf[1];
-    scan->second = (uint8_t)buf[2];
-    scan->milisecond = (uint16_t)buf[3];
-
-    for (int i = 0; i <= 2; i++)
-    {
-        scan->scannerid[0 + i] = buf[5 + i];
-    }
-
-    scan->status[0] = buf[8];
-
-    for (int i = 0; i <= 2; i++)
-    {
-        scan->padding[0 + i] = buf[9 + i];
-    }
+    scan_timestamp(ptr->scanner.timestamp, buf, &buf_pos);
+    scan_bytes_from_buf(ptr->scanner.scannerid, buf, &buf_pos, 3);
+    scan_bytes_from_buf(ptr->scanner.status, buf, &buf_pos, 1);
+    scan_bytes_from_buf(ptr->scanner.padding, buf, &buf_pos, 2);
 
     return 0;
 }
 
-int read_event(CTREvent *event, uint16_t len, FILE *fp)
+int read_event(CTRStruct *ptr, uint16_t len, FILE *fp)
 {
-    event->length = len;
-    event->type = 4;
+    ptr->type = EVENT;
+    ptr->event.length = len;
 
-    uint8_t buf[len - 4];
+    uint16_t buf_pos = 0;
+    uint8_t buf[len - 4]; // first 4 bytes of buf (type+length) already read from file
 
     if (fread(buf, 1, sizeof buf, fp) != sizeof buf)
         return -1;
 
-    event->id = buf[2] | buf[1] << 8 | buf[0] << 16;
+    // ptr->event.id = buf[2] | buf[1] << 8 | buf[0] << 16;
+    ptr->event.id = be32_to_cpu(buf);
+    buf_pos = buf_pos + 3;
 
-    for (int i = 0; i <= len - 4 - 3; i++)
-    {
-        event->parameters[0 + i] = buf[3 + i];
-    }
+    scan_bytes_from_buf(ptr->event.parameters, buf, &buf_pos, len - buf_pos - 4);
 
     return 0;
 }
@@ -202,55 +249,89 @@ int read_record_len_type(uint16_t *len, uint16_t *type, FILE *fp)
     return 0;
 }
 
-void print_header(CTRHeader *hdr)
+CTRStruct *add_record(uint16_t type, uint16_t lenght, FILE *fp)
 {
-    printf("Header\n");
-    printf("-----------------------\n");
-    printf("Record Lenght: %d\n", hdr->length);
-    printf("Record Type: %d\n", hdr->type);
-    printf("File Format Version: %s\n", hdr->version);
-    printf("PM recording version: %s\n", hdr->pm_version);
-    printf("PM recording revision: %s\n", hdr->pm_revision);
-    printf("Year: %d\n", hdr->year);
-    printf("Month: %d\n", hdr->month);
-    printf("Day: %d\n", hdr->day);
-    printf("Hour: %d\n", hdr->hour);
-    printf("Minute: %d\n", hdr->minute);
-    printf("Second: %d\n", hdr->second);
-    printf("NE user label: %s\n", hdr->ne_user_label);
-    printf("NE logical label: %s\n", hdr->ne_logical_label);
-}
-
-void print_scanner(CTRScanner *scan)
-{
-    printf("\n");
-    printf("Scanner\n");
-    printf("-----------------------\n");
-    printf("Record Lenght: %d\n", scan->length);
-    printf("Record Type: %d\n", scan->type);
-    printf("Hour: %d\n", scan->hour);
-    printf("Minute: %d\n", scan->minute);
-    printf("Second: %d\n", scan->second);
-    printf("Milisecond: %d\n", scan->milisecond);
-    printf("Scannerid: 0x%x 0x%x 0x%x\n", scan->scannerid[0], scan->scannerid[1], scan->scannerid[2]);
-    printf("Status: 0x%x\n", scan->status[0]);
-    printf("Padding Bytes: 0x%x 0x%x 0x%x\n", scan->padding[0], scan->padding[1], scan->padding[2]);
-}
-
-void print_event(CTREvent *event)
-{
-    printf("\n");
-    printf("Event\n");
-    printf("-----------------------\n");
-    printf("Record Lenght: %d\n", event->length);
-    printf("Record Type: %d\n", event->type);
-    printf("Event ID: %d\n", event->id);
-    printf("Event parameter part: ");
-    for (int i = 0; i <= event->length - 4 - 3; i++)
+    CTRStruct *node = malloc(sizeof(CTRStruct));
+    switch (type)
     {
-        printf("0x%x ", event->parameters[i]);
+    case HEADER:
+        read_header(node, lenght, fp);
+        return node;
+    case SCANNER:
+        read_scanner(node, lenght, fp);
+        return node;
+    case EVENT:
+        read_event(node, lenght, fp);
+        return node;
+    case FOOTER:
+        // read_footer(&footer, record_lenght, file);
+        // return node;
+    default:
+        printf("Record type not known");
+        return NULL;
     }
-    printf("\n");
+
+    return node;
+}
+
+void print_header(CTRStruct *ptr)
+{
+    printf("\nHeader (%d bytes):\n", ptr->header.length);
+    printf("{\n");
+    printf("timestamp: %s\n", ptr->header.date_time);
+    printf("file-format-version: %s\n", ptr->header.file_version);
+    printf("pm-recording-version: %s\n", ptr->header.pm_version);
+    printf("pm-recording-revision: %s\n", ptr->header.pm_revision);
+    printf("ne-user-label: %s\n", ptr->header.ne_user_label);
+    printf("ne-logical-name: %s\n", ptr->header.ne_logical_label);
+    printf("}\n");
+}
+
+void print_scanner(CTRStruct *ptr)
+{
+    printf("\nScanner (%d bytes):\n", ptr->scanner.length);
+    printf("{\n");
+    printf("timestamp: %s\n", ptr->scanner.timestamp);
+    printf("Scannerid: 0x%02x%02x%02x\n", ptr->scanner.scannerid[0], ptr->scanner.scannerid[1], ptr->scanner.scannerid[2]);
+    printf("Status: 0x%x\n", ptr->scanner.status[0]);
+    printf("Padding Bytes: 0x%02x%02x%02x\n", ptr->scanner.padding[0], ptr->scanner.padding[1], ptr->scanner.padding[2]);
+    printf("}\n");
+}
+
+void print_event(CTRStruct *ptr)
+{
+    printf("\nEvent (%d bytes):\n", ptr->event.length);
+    printf("{\n");
+    printf("Record Lenght: %d\n", ptr->event.length);
+    printf("Event ID: %d\n", ptr->event.id);
+    printf("Event parameter part: ");
+    for (int i = 0; i <= ptr->event.length - 4 - 3; i = i + 2)
+    {
+        printf("%02x%02x ", ptr->event.parameters[i], ptr->event.parameters[i + 1]);
+    }
+    printf("}\n");
+}
+
+void dump_records(CTRStruct *node)
+{
+    do
+    {
+        switch (node->type)
+        {
+        case HEADER:
+            print_header(node);
+            break;
+        case SCANNER:
+            print_scanner(node);
+            break;
+        case EVENT:
+            print_event(node);
+            break;
+        case FOOTER:
+            // print_footer(node);
+            break;
+        }
+    } while ((node = node->next) != NULL);
 }
 
 int main(void)
@@ -259,53 +340,67 @@ int main(void)
     file = fopen(filepath, "rb");
     if (file == NULL)
     {
-        printf("Error while opening the file.\n");
-        exit(0);
+        printf("ERROR: Opening the file.\n");
+        exit(1);
     }
-    fseek(file, 0L, SEEK_END);
-    int file_len = ftell(file);
-    fseek(file, 0L, SEEK_SET);
 
-    CTRHeader header = {0};
-    CTRScanner scanner = {0};
-    CTREvent event = {0};
-    CTRFooter footer = {0};
+    int file_lenght = get_file_lenght(file);
+    if (file_lenght != 0)
+    {
+        printf("DEBUG: Processing file with %d bytes\n", file_lenght);
+    }
+    else
+    {
+        printf("ERROR: File is empty");
+    }
+
+    CTRStruct *head = NULL;
+    CTRStruct *tail = NULL;
+    CTRStruct *node = NULL;
 
     int num_records = 0;
-    while (file_len > 0 && num_records < 10)
+    while (file_lenght > 0 && num_records < 5)
     {
         uint16_t record_lenght = 0;
         uint16_t record_type = 255;
         read_record_len_type(&record_lenght, &record_type, file);
-
-        switch (record_type)
+        if (record_lenght <= 0 || record_lenght > file_lenght)
         {
-        case 0:
-            read_header(&header, record_lenght, file);
-            print_header(&header);
-            break;
-        case 3:
-            read_scanner(&scanner, record_lenght, file);
-            print_scanner(&scanner);
-            break;
-        case 4:
-            read_event(&event, record_lenght, file);
-            print_event(&event);
-            break;
-        case 5:
-            /* read_footer(&footer, record_lenght, file);
-            print_footer(&footer); */
-            break;
-        default:
-            printf("Record type not known");
-            break;
+            printf("ERROR: Record lenght '%d' not valid\n", record_lenght);
+            exit(1);
         }
+        if (RecordTypeValid(record_type) != 1)
+        {
+            printf("ERROR: Record type '%d' not known\n", record_type);
+            exit(1);
+        }
+        printf("DEBUG: Record type %d found with %d bytes\n", record_type, record_lenght);
 
-        file_len = file_len - record_lenght;
+        node = add_record(record_type, record_lenght, file);
+        if (node == NULL)
+        {
+            printf("ERROR: Adding record to linked list.\n");
+            exit(1);
+        }
+        // printf("DEBUG: Current file position, %d\n", get_file_pos(file));
+
+        if (record_type == HEADER)
+        {
+            head = tail = node;
+        }
+        else
+        {
+            tail->next = node;
+            tail = node;
+        }
         num_records++;
+        file_lenght = file_lenght - record_lenght;
     }
+    printf("DEBUG: Total %d records processed\n", num_records);
 
     fclose(file);
+
+    dump_records(head);
 
     return 0;
 }
